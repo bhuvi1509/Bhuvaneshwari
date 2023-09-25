bc75cb3f87771583ec097606b5681591f0dfd249
bhuvaneshwari <143331329+bhuvi1509@users.noreply.github.com>
2023-09-24 19:13:02 -0700

    Add files via upload

---
+++ Marks_dictionary.py
@@ -0,0 +1,11 @@
+marks_dictionary = {}
+marks_dictionary["Tamil-3"] = 72
+marks_dictionary["English-3"] = 70
+marks_dictionary["Resource Mangaement Techniques"] = 80
+marks_dictionary["Data Structures & Computer Algorithms"] = 82
+marks_dictionary["digital principles & computer organistion"] = 72
+marks_dictionary["multimedia lab"] = 92
+
+for subject in marks_dictionary:
+    mark = marks_dictionary[subject]
+    print(subject,mark)
